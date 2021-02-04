d0928361376638a59a6959c21f55a7b9aa51f4e0
Qui <quivan3396@yahoo.com>
2021-02-04 16:07:20 -0500

    feat: render random articles

--- client/components/Tools.js
+++ client/components/Tools.js
@@ -14,30 +14,45 @@ import '../../public/css/tools.css'
 
 export class Tools extends React.Component {
   render() {
+    const shuffle = o => {
+      for (
+        var j, x, i = o.length;
+        i;
+        j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+      );
+      return o
+    }
+    let count = 0
     return (
       <div>
         <h1>Tools</h1>
         <div className="article-container">
-          {articles.map(article => {
-            return (
-              <Card className="article-card" key={article.id}>
-                <a href={article.link} className="articleLink">
-                  <CardImg
-                    top
-                    width="100%"
-                    height="50%"
-                    src={article.url}
-                    alt="Card image cap"
-                  />
-                  <CardBody className="article-body">
-                    <CardTitle tag="h3">{article.title}</CardTitle>
+          {shuffle(articles).map(article => {
+            while (count < 6) {
+              count++
+              return (
+                <Card className="article-card" key={article.id}>
+                  <a
+                    href={article.link}
+                    target="_blank"
+                    className="articleLink"
+                  >
+                    <CardImg
+                      top
+                      width="100%"
+                      height="50%"
+                      src={article.url}
+                      alt="Card image cap"
+                    />
+                    <CardBody className="article-body">
+                      <CardTitle tag="h3">{article.title}</CardTitle>
 
-                    <CardText>{article.description}</CardText>
-                    {/* <Button >See More</Button> */}
-                  </CardBody>
-                </a>
-              </Card>
-            )
+                      <CardText>{article.description}</CardText>
+                    </CardBody>
+                  </a>
+                </Card>
+              )
+            }
           })}
         </div>
       </div>
