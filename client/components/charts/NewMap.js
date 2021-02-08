d0244c81c0981566d5f3b6ecba845995816ec31b
Dan Chong <DChong0618@gmail.com>
2021-02-08 14:50:13 -0600

    Render data in newMap

--- client/components/charts/NewMap.js
+++ client/components/charts/NewMap.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 const USMap = props => {
   const [statesData, setStatesData] = useState(null)
+  const userData = props.data
 
   // This should only run once due to the [] arg for the dependencies.
   useEffect(() => {
@@ -22,16 +23,18 @@ const USMap = props => {
 
   return (
     <svg viewBox="0 0 960 600">
-      {statesData.map((stateData, index) => (
+      {statesData.map(stateData => (
         <path
           className="someCSSClass"
           style={{cursor: 'pointer', fill: 'black'}}
-          key={index}
+          key={stateData.id}
           stroke="#fff"
           strokeWidth="1px"
           d={stateData.shape}
           onMouseOver={event => {
             event.target.style.fill = 'red'
+            let user = userData.filter(data => data.state === stateData.name)
+            console.log('This is user -> ', user)
           }}
           onMouseOut={event => {
             event.target.style.fill = 'black'
