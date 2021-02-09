8e8d027dbad7a957cbff29ecbb8d5b086104030e
Qui <quivan3396@yahoo.com>
2021-02-09 17:45:33 -0500

    feat: added average salaries for males and females

--- client/components/charts/NewMap.js
+++ client/components/charts/NewMap.js
@@ -2,8 +2,10 @@ import React, {useState, useEffect} from 'react'
 
 const USMap = props => {
   const [statesData, setStatesData] = useState(null)
+  const [female, setFemaleSalary] = useState(0)
+  const [male, setMaleSalary] = useState(0)
   const userData = props.data
-
+  // console.log('outside return' , female)
   // This should only run once due to the [] arg for the dependencies.
   useEffect(() => {
     ;(async () => {
@@ -22,26 +24,58 @@ const USMap = props => {
   }
 
   return (
-    <svg viewBox="0 0 960 600">
-      {statesData.map(stateData => (
-        <path
-          className="someCSSClass"
-          style={{cursor: 'pointer', fill: 'black'}}
-          key={stateData.id}
-          stroke="#fff"
-          strokeWidth="1px"
-          d={stateData.shape}
-          onMouseOver={event => {
-            event.target.style.fill = 'red'
-            let user = userData.filter(data => data.state === stateData.name)
-            console.log('This is user -> ', user)
-          }}
-          onMouseOut={event => {
-            event.target.style.fill = 'black'
-          }}
-        />
-      ))}
-    </svg>
+    <>
+      <h1>
+        Female:{' '}
+        {isNaN(female)
+          ? 'No Female Data'
+          : female.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            })}
+      </h1>
+      <h1>
+        Male:{' '}
+        {isNaN(male)
+          ? 'No Male Data'
+          : male.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            })}
+      </h1>
+      <svg viewBox="0 0 960 600">
+        {statesData.map(stateData => (
+          <path
+            className="someCSSClass"
+            style={{cursor: 'pointer', fill: 'black'}}
+            key={stateData.id}
+            stroke="#fff"
+            strokeWidth="1px"
+            d={stateData.shape}
+            onMouseOver={event => {
+              event.target.style.fill = 'red'
+              let user = userData.filter(data => data.state === stateData.name)
+              let females = user.filter(f => f.gender === 'Female')
+              let femaleTotal = females
+                .map(f => f.salary)
+                .reduce((accu, val) => accu + +val, 0)
+              let femaleAve = femaleTotal / females.length
+              setFemaleSalary(femaleAve)
+
+              let males = user.filter(m => m.gender === 'Male')
+              let maleTotal = males
+                .map(m => m.salary)
+                .reduce((accu, val) => accu + +val, 0)
+              let maleAve = maleTotal / males.length
+              setMaleSalary(maleAve)
+            }}
+            onMouseOut={event => {
+              event.target.style.fill = 'black'
+            }}
+          />
+        ))}
+      </svg>
+    </>
   )
 }
 
