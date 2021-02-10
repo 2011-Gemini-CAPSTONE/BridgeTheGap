601d15f88cc934038244cfd931808c195c60e334
Dan Chong <DChong0618@gmail.com>
2021-02-10 14:57:05 -0600

    Moved legend on top of map/

--- client/components/charts/NewMap.js
+++ client/components/charts/NewMap.js
@@ -28,6 +28,51 @@ const USMap = props => {
   return (
     <>
       <div className="map">
+        <div className="dataMap">
+          <svg viewBox="0 0 1080 650">
+            {statesData.map(stateData => (
+              <path
+                className="someCSSClass"
+                style={{cursor: 'pointer', fill: '#87BDDC'}}
+                key={stateData.id}
+                stroke="#fff"
+                strokeWidth="1px"
+                d={stateData.shape}
+                onMouseOver={event => {
+                  event.target.style.fill = '#083E80'
+                  let user = userData.filter(
+                    data => data.state === stateData.name
+                  )
+                  let femalesFilter = user.filter(f => f.gender === 'Female')
+                  let females = femalesFilter.filter(
+                    f => f.jobTitle === 'Software Engineer'
+                  )
+                  let femaleTotal = females
+                    .map(f => f.salary)
+                    .reduce((accu, val) => accu + +val, 0)
+                  let femaleAve = femaleTotal / females.length
+                  setFemaleSalary(femaleAve)
+
+                  let malesFilter = user.filter(m => m.gender === 'Male')
+                  let males = malesFilter.filter(
+                    m => m.jobTitle === 'Software Engineer'
+                  )
+                  let maleTotal = males
+                    .map(m => m.salary)
+                    .reduce((accu, val) => accu + +val, 0)
+                  let maleAve = maleTotal / males.length
+                  setMaleSalary(maleAve)
+
+                  console.log('This is female -> ', females)
+                  console.log('This is male -> ', males)
+                }}
+                onMouseOut={event => {
+                  event.target.style.fill = '#87BDDC'
+                }}
+              />
+            ))}
+          </svg>
+        </div>
         <div className="dataBox">
           <div className="femaleData">
             <h4 className="femaleText">
@@ -58,56 +103,13 @@ const USMap = props => {
             </p>
           </div>
         </div>
-        <svg viewBox="0 0 1080 650">
-          {statesData.map(stateData => (
-            <path
-              className="someCSSClass"
-              style={{cursor: 'pointer', fill: '#87BDDC'}}
-              key={stateData.id}
-              stroke="#fff"
-              strokeWidth="1px"
-              d={stateData.shape}
-              onMouseOver={event => {
-                event.target.style.fill = '#083E80'
-                let user = userData.filter(
-                  data => data.state === stateData.name
-                )
-                let femalesFilter = user.filter(f => f.gender === 'Female')
-                let females = femalesFilter.filter(
-                  f => f.jobTitle === 'Software Engineer'
-                )
-                let femaleTotal = females
-                  .map(f => f.salary)
-                  .reduce((accu, val) => accu + +val, 0)
-                let femaleAve = femaleTotal / females.length
-                setFemaleSalary(femaleAve)
-
-                let malesFilter = user.filter(m => m.gender === 'Male')
-                let males = malesFilter.filter(
-                  m => m.jobTitle === 'Software Engineer'
-                )
-                let maleTotal = males
-                  .map(m => m.salary)
-                  .reduce((accu, val) => accu + +val, 0)
-                let maleAve = maleTotal / males.length
-                setMaleSalary(maleAve)
-
-                console.log('This is female -> ', females)
-                console.log('This is male -> ', males)
-              }}
-              onMouseOut={event => {
-                event.target.style.fill = '#87BDDC'
-              }}
-            />
-          ))}
-        </svg>
-      </div>
-      <div>
-        <p id="disclaimerData">
-          {' '}
-          * Data is based off of 2020 Female Salaries in Tech and Reddit Tech
-          Salary Sheet 2020
-        </p>
+        <div>
+          <p id="disclaimerData">
+            {' '}
+            * Data is based off of 2020 Female Salaries in Tech and Reddit Tech
+            Salary Sheet 2020
+          </p>
+        </div>
       </div>
     </>
   )
