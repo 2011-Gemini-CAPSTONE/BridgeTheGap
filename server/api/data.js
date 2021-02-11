8037fde7bbd9dc87eb6e8148784dc4203fdfd79b
James Yon <jamesyon55@gmail.com>
2021-02-11 11:55:20 -0600

    created routes for users and data

--- server/api/data.js
+++ server/api/data.js
@@ -1,12 +1,61 @@
 const router = require('express').Router()
 const Data = require('../db/models/data')
 
+const isAdminMiddleware = (req, res, next) => {
+  const currentUser = req.user
+  if (currentUser && currentUser.isAdmin) {
+    next()
+  } else {
+    const error = new Error('Not Admin')
+    error.status = 401
+    next(error)
+  }
+}
+
+const isUserMiddleware = (req, res, next) => {
+  const currentUser = req.user
+  if (currentUser && currentUser === req.params.id) {
+    next()
+  } else {
+    const error = new Error('Please Sign In')
+    error.status = 401
+    next(error)
+  }
+}
+
 router.get('/', async (req, res, next) => {
   try {
     const data = await Data.findAll()
     res.json(data)
   } catch (err) {
     next(err)
+  }
+})
+
+router.post('/', isUserMiddleware, async (req, res, next) => {
+  try {
+    const data = await Data.create(req.body)
+    res.json(data)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', isUserMiddleware, async (req, res, next) => {
+  try {
+    const data = await Data.findByPk(req.params.id)
+    res.send(await data.update(req.body))
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', isUserMiddleware, async (req, res, next) => {
+  try {
+    const data = await Data.findByPk(req.params.id)
+    await data.destroy()
+  } catch (error) {
+    next(error)
   }
 })
 
--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,28 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+const isAdminMiddleware = (req, res, next) => {
+  const currentUser = req.user
+  if (currentUser && currentUser.isAdmin) {
+    next()
+  } else {
+    const error = new Error('Not Admin')
+    error.status = 401
+    next(error)
+  }
+}
+
+const isUserMiddleware = (req, res, next) => {
+  const currentUser = req.user
+  if (currentUser && currentUser === req.params.id) {
+    next()
+  } else {
+    const error = new Error('Please Sign In')
+    error.status = 401
+    next(error)
+  }
+}
+
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -13,5 +35,32 @@ router.get('/', async (req, res, next) => {
     res.json(users)
   } catch (err) {
     next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const users = await User.create(req.body)
+    res.json(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', isUserMiddleware, async (req, res, next) => {
+  try {
+    const users = await User.findByPk(req.params.id)
+    res.send(await users.update(req.body))
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', isUserMiddleware, async (req, res, next) => {
+  try {
+    const users = await User.findByPk(req.params.id)
+    await users.destroy()
+  } catch (error) {
+    next(error)
   }
 })
