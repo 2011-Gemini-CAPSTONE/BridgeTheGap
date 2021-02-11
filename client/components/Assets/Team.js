e19232c22de43a84c31ea754790c7ec59d2d623c
Dan Chong <DChong0618@gmail.com>
2021-02-11 15:17:02 -0600

    css for salary form

---
+++ client/components/Assets/Team.js
@@ -0,0 +1,24 @@
+const teamNames = [
+  'Technical Program Management',
+  'Software Engineering Management',
+  'Software Engineering',
+  'Sales',
+  'Recruiting',
+  'Product Management',
+  'Product Design',
+  'Operations',
+  'Network, Security & IT',
+  'Misc',
+  'Mechanical Engineering',
+  'Marketing Ops',
+  'Marketing',
+  'Human Resources',
+  'Hardware Engineering',
+  'Data Science',
+  'Creative',
+  'Business Analytics',
+  'Agile',
+  'Accounting'
+]
+
+export default teamNames
